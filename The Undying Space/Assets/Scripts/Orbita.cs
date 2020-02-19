eee3a6f40c6aba8f2280fc527f3dfaa9b8b063af
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-02-19 16:39:54 -0300

    melhorar a linha da orbita

--- The Undying Space/Assets/Scripts/Orbita.cs
+++ The Undying Space/Assets/Scripts/Orbita.cs
@@ -1,41 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Orbita : MonoBehaviour
 {
     public float RotateSpeed = 5f;
     public float Radius = 0.1f;
-
     public Transform CentroDaRotacao;
+
     private float _angle;
+    private LineRenderer _lineRenderer;
+
+    private void Start()
+    {
+        _lineRenderer = gameObject.AddComponent<LineRenderer>();
+        _lineRenderer.sortingOrder = 1;
+        _lineRenderer.startColor = Color.white;
+        _lineRenderer.endColor = Color.white;
+        _lineRenderer.widthMultiplier = 0.300f;
+        _lineRenderer.material = Resources.Load("Materials/MaterialWhite", typeof(Material)) as Material;
+        _lineRenderer.loop = true;
+
+    }
 
     private void Update()
     {
+        DesenharOrbita();
+        Orbitar();
+    }
 
+    private void Orbitar()
+    {
         _angle += RotateSpeed * Time.deltaTime;
         var offset = new Vector3(Mathf.Sin(_angle), Mathf.Cos(_angle), 0) * Radius;
         transform.position = CentroDaRotacao.position + offset;
-
-        DebugDrawPolygon(polygonCenter, polygonRadius, numberOfSides);
     }
 
-    public int numberOfSides;
-    public float polygonRadius;
-    public Vector2 polygonCenter;
-
-    void DebugDrawPolygon(Vector2 center, float radius, int numSides)
+    void DesenharOrbita()
     {
-        Vector2 startCorner = new Vector2(radius, 0) + center;
-        Vector2 previousCorner = startCorner;
-        for (int i = 1; i < numSides; i++)
+        int pontos = 50;
+        float angle = 2 * Mathf.PI / pontos;
+        _lineRenderer.positionCount = pontos;
+
+        for (int i = 0; i < pontos; i++)
         {
-            float cornerAngle = 2f * Mathf.PI / (float)numSides * i;
-            Vector2 currentCorner = new Vector2(Mathf.Cos(cornerAngle) * radius, Mathf.Sin(cornerAngle) * radius) + center;
-            Debug.DrawLine(currentCorner, previousCorner);
-            previousCorner = currentCorner;
+            Matrix4x4 rotationMatrix = new Matrix4x4(new Vector4(Mathf.Cos(angle * i), Mathf.Sin(angle * i), 0, 0),
+                                                     new Vector4(-1 * Mathf.Sin(angle * i), Mathf.Cos(angle * i), 0, 0),
+                                       new Vector4(0, 0, 1, 0),
+                                       new Vector4(0, 0, 0, 1));
+            Vector3 initialRelativePosition = new Vector3(0, Radius, 0);
+            _lineRenderer.SetPosition(i, CentroDaRotacao.position + rotationMatrix.MultiplyPoint(initialRelativePosition));
         }
-        // Finaliza conectando o ultimo canto com o primeiro.
-        Debug.DrawLine(startCorner, previousCorner);
     }
+
+    // O debug é mais facil com linha inicial e linha final rsrs.
+    //void DesenharOrbita(Vector2 center, float radius, int numSides)
+    //{
+    //    Vector2 startCorner = new Vector2(radius, 0) + center;
+    //    Vector2 previousCorner = startCorner;
+    //    for (int i = 1; i < numSides; i++)
+    //    {
+    //        float cornerAngle = 2f * Mathf.PI / numSides * i;
+    //        Vector2 currentCorner = new Vector2(Mathf.Cos(cornerAngle) * radius, Mathf.Sin(cornerAngle) * radius) + center;
+    //        Debug.DrawLine(currentCorner, previousCorner);
+    //        _lineRenderer.SetPosition(i, currentCorner);
+    //        previousCorner = currentCorner;
+    //    }
+    //    // Finaliza conectando o ultimo canto com o primeiro.
+    //    Debug.DrawLine(startCorner, previousCorner);
+    //    //_lineRender.SetPosition(51, previousCorner);
+    //
 }
