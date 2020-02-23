966e6d91f6b27adea0d8b69adcd5bca3ed0a8806
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2020-02-22 21:15:17 -0300

    melhorar zoomcontrol

--- The Undying Space/Assets/Scripts/Camera/ZoomControl.cs
+++ The Undying Space/Assets/Scripts/Camera/ZoomControl.cs
@@ -1,52 +1,40 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ZoomControl : MonoBehaviour
 {
-
-    public float ZoomSize = 60;
+    private const string MOUSE = "Mouse ScrollWheel";
+    private float _zoomVel = 80f;
     private Camera _camera;
-
-    void Start()
-    {
-        _camera = GetComponent<Camera>();
-    }
+    void Start() => _camera = GetComponent<Camera>();
 
     void Update()
     {
-        ZoomSize -= Input.GetAxis("Mouse ScrollWheel") * 10;
-        //if (ZoomSize <= 5)
-        //    ZoomSize = 5;
-        //else if (ZoomSize >= 100)
-        //    ZoomSize = 100;
-        //_camera.orthographicSize = ZoomSize;
-        // Scroll forward
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
-        {
-            ZoomOrthoCamera(Camera.main.ScreenToWorldPoint(Input.mousePosition), 1);
-        }
 
-        // Scoll back
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
-        {
-            ZoomOrthoCamera(Camera.main.ScreenToWorldPoint(Input.mousePosition), -1);
-        }
+        if (Input.GetAxis(MOUSE) > 0 || Input.GetAxis(MOUSE) < 0)
+            ZoomOrthoCamera(_camera.ScreenToWorldPoint(Input.mousePosition), Input.GetAxis(MOUSE));
+
+        //if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        //{
+        //    ZoomOrthoCamera(_camera.ScreenToWorldPoint(Input.mousePosition), ZoomSize);
+        //}
+
+        //// Scoll back
+        //if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        //{
+        //    ZoomOrthoCamera(_camera.ScreenToWorldPoint(Input.mousePosition), ZoomSize);
+        //}
     }
 
     // Ortographic camera zoom towards a point (in world coordinates). Negative amount zooms in, positive zooms out
-    // TODO: when reaching zoom limits, stop camera movement as well
     void ZoomOrthoCamera(Vector3 zoomTowards, float amount)
     {
         // Calculate how much we will have to move towards the zoomTowards position
-        float multiplier = (1.0f / _camera.orthographicSize * amount);
+        float multiplier = (_zoomVel / _camera.orthographicSize * amount);
 
         // Move camera
         transform.position += (zoomTowards - transform.position) * multiplier;
-
         // Zoom camera
-        _camera.orthographicSize -= amount;
-
+        _camera.orthographicSize -= amount * _zoomVel;
         // Limit zoom
         _camera.orthographicSize = Mathf.Clamp(_camera.orthographicSize, 20, 400);
     }
